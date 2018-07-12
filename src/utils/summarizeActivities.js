946f5b29c9088fd4a47b8e56252c2b2878ddd4d3
Eric Giovanola <ericgio@gmail.com>
2018-07-12 01:06:17 -0700

    Fix activity summarization

--- src/utils/summarizeActivities.js
+++ src/utils/summarizeActivities.js
@@ -3,37 +3,40 @@ import moment from 'moment-timezone';
 const INCLUSIVE = '[]';
 const PERIODS = ['week', 'month', 'year'];
 
-const summary = {};
-PERIODS.forEach((period) => {
-  summary[period] = {
-    count: 0,
-    sumDistance: 0,
-  };
-});
-
 /**
  * Summarizes all the activities for a given range of time into count and total
  * distance for the current week, month, and year.
  */
-export default (activities) => activities.reduce((accum, activity) => {
-  const {distance, startDate, timezone} = activity;
-  const start = moment(startDate).tz(timezone);
+export default (activities) => {
+  const summary = {};
 
   PERIODS.forEach((period) => {
-    if (
-      start.isBetween(
-        moment().tz(timezone).startOf(period),
-        moment().tz(timezone).endOf(period),
-        null,
-        INCLUSIVE,
-      )
-    ) {
-      accum[period] = {
-        count: accum[period].count + 1,
-        sumDistance: accum[period].sumDistance + +distance,
-      };
-    }
+    summary[period] = {
+      count: 0,
+      sumDistance: 0,
+    };
   });
 
-  return accum;
-}, summary);
+  return activities.reduce((accum, activity) => {
+    const {distance, startDate, timezone} = activity;
+    const start = moment(startDate).tz(timezone);
+
+    PERIODS.forEach((period) => {
+      if (
+        start.isBetween(
+          moment().tz(timezone).startOf(period),
+          moment().tz(timezone).endOf(period),
+          null,
+          INCLUSIVE,
+        )
+      ) {
+        accum[period] = {
+          count: accum[period].count + 1,
+          sumDistance: accum[period].sumDistance + +distance,
+        };
+      }
+    });
+
+    return accum;
+  }, summary);
+};
