4b1357d9bfe28ed50071e36a816f515591fac703
Eric Giovanola <ericgio@gmail.com>
2018-07-12 01:34:26 -0700

    Fix start time when importing garmin activity

--- src/queries/garminActivityQuery.js
+++ src/queries/garminActivityQuery.js
@@ -32,25 +32,20 @@ function garminUrlToActivity(activity) {
     startTimeLocal,
   } = activity.summaryDTO;
 
+  const timezone = activity.timeZoneUnitDTO.timeZone;
+
   return {
     activityType: activity.activityTypeDTO.typeKey,
     avgHr: averageHR,
-    // TODO: Accept decimal values?
     calories: calories && Math.round(calories),
     distance: metersToMiles(distance),
-    // TODO: Accept decimal values?
     duration: Math.round(duration),
     elevationGain: metersToFeet(elevationGain),
     elevationLoss: metersToFeet(elevationLoss),
     garminActivityId: activity.activityId,
     maxHr: maxHR,
-    startDate: moment(startTimeLocal).format(),
-    timezone: activity.timeZoneUnitDTO.timeZone,
-
-    // TODO: Add these models to the schema.
-    // device: {},
-    // laps: splits.lapDTOs,
-    // tracks: [],
+    startDate: moment(startTimeLocal).tz(timezone).format(),
+    timezone,
   };
 }
 
