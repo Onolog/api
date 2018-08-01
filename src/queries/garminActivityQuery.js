a6f3bd58b367673ce70bdc287ea477218464d541
Eric Giovanola <ericgio@gmail.com>
2018-08-01 14:17:59 -0700

    Move garminUrlToActivity function to util file

--- src/queries/garminActivityQuery.js
+++ src/queries/garminActivityQuery.js
@@ -1,53 +1,9 @@
 import {GraphQLID, GraphQLNonNull} from 'graphql';
 import fetch from 'isomorphic-fetch';
-import moment from 'moment-timezone';
 
 import {GarminActivityType} from '../types';
-import {FEET_PER_METER, GARMIN_ACTIVITY_URL, METERS_PER_MILE} from '../constants';
-
-const metersToMiles = (distanceInMeters, precision=2) => {
-  const miles = distanceInMeters / METERS_PER_MILE;
-
-  // Round to x decimal places.
-  // See:
-  //  - https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript
-  //  - https://stackoverflow.com/questions/10015027/javascript-tofixed-not-rounding/32605063#32605063
-  return +(Math.round(+(miles + 'e' + precision)) + 'e' + -precision);
-};
-
-const metersToFeet = (distanceInMeters) => (
-  +(Math.round(distanceInMeters * FEET_PER_METER))
-);
-
-// Normalizes activity data pulled from Garmin's endpoints.
-function garminUrlToActivity(activity) {
-  const {
-    averageHR,
-    calories,
-    distance,
-    duration,
-    elevationGain,
-    elevationLoss,
-    maxHR,
-    startTimeLocal,
-  } = activity.summaryDTO;
-
-  const timezone = activity.timeZoneUnitDTO.timeZone;
-
-  return {
-    activityType: activity.activityTypeDTO.typeKey,
-    avgHr: averageHR,
-    calories: calories && Math.round(calories),
-    distance: metersToMiles(distance),
-    duration: Math.round(duration),
-    elevationGain: metersToFeet(elevationGain),
-    elevationLoss: metersToFeet(elevationLoss),
-    garminActivityId: activity.activityId,
-    maxHr: maxHR,
-    startDate: moment.tz(startTimeLocal, timezone).format(),
-    timezone,
-  };
-}
+import {GARMIN_ACTIVITY_URL} from '../constants';
+import garminUrlToActivity from '../utils/garminUrlToActivity';
 
 const garminActivityQuery = {
   type: new GraphQLNonNull(GarminActivityType),
---
+++ src/utils/garminUrlToActivity.js
@@ -0,0 +1,46 @@
+import moment from 'moment-timezone';
+import {FEET_PER_METER, METERS_PER_MILE} from '../constants';
+
+const metersToMiles = (distanceInMeters, precision=2) => {
+  const miles = distanceInMeters / METERS_PER_MILE;
+
+  // Round to x decimal places.
+  // See:
+  //  - https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript
+  //  - https://stackoverflow.com/questions/10015027/javascript-tofixed-not-rounding/32605063#32605063
+  return +(Math.round(+(miles + 'e' + precision)) + 'e' + -precision);
+};
+
+const metersToFeet = (distanceInMeters) => (
+  +(Math.round(distanceInMeters * FEET_PER_METER))
+);
+
+// Normalizes activity data pulled from Garmin's endpoints.
+export default function garminUrlToActivity(activity) {
+  const {
+    averageHR,
+    calories,
+    distance,
+    duration,
+    elevationGain,
+    elevationLoss,
+    maxHR,
+    startTimeLocal,
+  } = activity.summaryDTO;
+
+  const timezone = activity.timeZoneUnitDTO.timeZone;
+
+  return {
+    activityType: activity.activityTypeDTO.typeKey,
+    avgHr: averageHR,
+    calories: calories && Math.round(calories),
+    distance: metersToMiles(distance),
+    duration: Math.round(duration),
+    elevationGain: metersToFeet(elevationGain),
+    elevationLoss: metersToFeet(elevationLoss),
+    garminActivityId: activity.activityId,
+    maxHr: maxHR,
+    startDate: moment.tz(startTimeLocal, timezone).format(),
+    timezone,
+  };
+}
