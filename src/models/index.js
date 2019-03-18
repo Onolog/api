85ee3198fc61875aad043d13cf9638706168bf16
Eric Giovanola <ericgio@gmail.com>
2019-03-18 12:19:29 -0700

    Upgrade Sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,6 @@ const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   dialect: 'mysql',
   host: DB_HOST,
   port: DB_PORT,
-  operatorsAliases: false,
   pool: {
     max: 5,
     min: 0,
