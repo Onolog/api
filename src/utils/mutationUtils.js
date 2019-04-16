6adf37c20f5ad88d10a96f7dc2534f01c2bdcff1
Eric Giovanola <ericgio@gmail.com>
2019-04-16 15:04:42 -0700

    Fix use of `findById`

--- src/utils/mutationUtils.js
+++ src/utils/mutationUtils.js
@@ -3,7 +3,7 @@ import assertCanEdit from './assertCanEdit';
 export const createRecord = (Model, options) => async (root, args, context) => {
   try {
     const instance = await Model.create(args.input);
-    return Model.findById(instance.id, options);
+    return Model.findByPk(instance.id, options);
   } catch (err) {
     throw Error(err);
   }
@@ -11,13 +11,13 @@ export const createRecord = (Model, options) => async (root, args, context) => {
 
 export const updateRecord = (Model, options) => async (root, args, context) => {
   try {
-    const instance = await Model.findById(args.id);
+    const instance = await Model.findByPk(args.id);
     assertCanEdit(context, instance.userId);
     await instance.update(args.input);
 
     // Re-fetch the record to make sure we have the right associations.
     // TODO: How to make this more efficient?
-    return await Model.findById(args.id, options);
+    return await Model.findByPk(args.id, options);
   } catch (err) {
     throw Error(err);
   }
@@ -26,7 +26,7 @@ export const updateRecord = (Model, options) => async (root, args, context) => {
 export const deleteRecord = (Model, options) => (
   async (root, { id }, context) => {
     try {
-      const instance = await Model.findById(id);
+      const instance = await Model.findByPk(id);
       assertCanEdit(context, instance.userId);
       await instance.destroy();
       return id;
